c2a1dca45b93d4c3fb4cf9deaab3def8763ef2bb
ritwizsinha <ritwizsinha0@gmail.com>
2021-11-25 17:47:58 +0530

    Added code and contents and created a soft link for content
    Signed-off-by: ritwizsinha <ritwizsinha0@gmail.com>

---
+++ config.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  url: 'https://ritwiz.dev/',
+  pathPrefix: '/',
+  title: 'ritwiz.dev',
+  subtitle: 'cat Life | grep "important-stuff"',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Tags',
+      path: '/tags'
+    }
+  ],
+  author: {
+    name: 'ritwiz.dev',
+    photo: '/photo.png',
+    // eslint-disable-next-line quotes
+    bio: `cat life | grep important stuff`,
+    contacts: {
+      email: 'ritwizsinha0@gmail.com',
+      facebook: '',
+      telegram: '',
+      twitter: 'sinharitwiz',
+      github: 'ritwizsinha',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'ritwizsinha',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    }
+  }
+};
