7d8e2c97627b984fa3aa3348e914549396f2e812
ritwizsinha <ritwizsinha0@gmail.com>
2021-11-30 17:19:03 +0530

    Updated about me
    Signed-off-by: ritwizsinha <ritwizsinha0@gmail.com>

---
+++ .cache/sync-requires.js
@@ -0,0 +1,18 @@
+
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": (preferDefault(require("/home/ritwiz/Desktop/github.com/ritwizsinha.github.io/.cache/dev-404-page.js"))),
+  "component---src-templates-categories-list-template-js": (preferDefault(require("/home/ritwiz/Desktop/github.com/ritwizsinha.github.io/src/templates/categories-list-template.js"))),
+  "component---src-templates-category-template-js": (preferDefault(require("/home/ritwiz/Desktop/github.com/ritwizsinha.github.io/src/templates/category-template.js"))),
+  "component---src-templates-index-template-js": (preferDefault(require("/home/ritwiz/Desktop/github.com/ritwizsinha.github.io/src/templates/index-template.js"))),
+  "component---src-templates-not-found-template-js": (preferDefault(require("/home/ritwiz/Desktop/github.com/ritwizsinha.github.io/src/templates/not-found-template.js"))),
+  "component---src-templates-page-template-js": (preferDefault(require("/home/ritwiz/Desktop/github.com/ritwizsinha.github.io/src/templates/page-template.js"))),
+  "component---src-templates-post-template-js": (preferDefault(require("/home/ritwiz/Desktop/github.com/ritwizsinha.github.io/src/templates/post-template.js"))),
+  "component---src-templates-tag-template-js": (preferDefault(require("/home/ritwiz/Desktop/github.com/ritwizsinha.github.io/src/templates/tag-template.js"))),
+  "component---src-templates-tags-list-template-js": (preferDefault(require("/home/ritwiz/Desktop/github.com/ritwizsinha.github.io/src/templates/tags-list-template.js")))
+}
+
---
+++ blog/content/pages/about.md
@@ -0,0 +1,7 @@
+---
+title: "About me"
+template: "page"
+socialImage: "/media/image-2.jpg"
+---
+
+A CS Student who was lured into this stream because of peer pressure, but now likes to talk at length about Algorithms, Distributed Systems, Databases and creating systems from scratch whenever given the chance.
